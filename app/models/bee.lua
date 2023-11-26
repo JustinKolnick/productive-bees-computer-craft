b9871b9ae737feaf1d255e883524db968a39cfe9
Justin <justinkolnick@gmail.com>
2023-11-26 01:51:16 -0500

    treat nil stat values as zero, since the colony builder expects to perform arithmetic on these values

--- app/models/bee.lua
+++ app/models/bee.lua
@@ -4,9 +4,9 @@ Bee.__index = Bee
 function Bee.new(args)
     local self = setmetatable({}, Bee)
     self.name = args.Name
-    self.health = args.Health
-    self.endurance = args.Endurance
-    self.productivity = args.Productivity
+    self.health = args.Health or 0
+    self.endurance = args.Endurance or 0
+    self.productivity = args.Productivity or 0
 
     return self
 end
